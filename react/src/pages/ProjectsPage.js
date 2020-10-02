c550edfcb995940adcbd439d3bc8ca072cb29161
Sluggter <jlugton@student.unimelb.edu.au>
2020-10-02 11:19:06 +1000

    projects page

--- react/src/pages/ProjectsPage.js
+++ react/src/pages/ProjectsPage.js
@@ -1,13 +1,75 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
-export default function HomePage() {
-  return (
-    <div className="container">
-      <h1>Home Page</h1>
-      <p>
-        here's the projects page
-      </p>
-    </div>
-  )
+class ProjectsPage extends React.Component {
+
+    constructor() {
+        super();
+        this.state = {
+            project: "none"
+        };
+    }
+
+    handleChange = (event) => {
+        const {name, value} = event.target;
+        this.setState({
+            [name]: value
+        })
+    }
+
+
+    render() {
+        const mouseHover = {
+            color: "blue"
+        }
+        const clicked = {
+            color: "#cdcdcd"
+        }
+
+        var project_desc = null;
+        if(this.state.project === "1"){
+            project_desc = "This is a description for project 1";
+        } else if(this.state.project === "2"){
+            project_desc = "This is a description for project 2";
+        } else if(this.state.project === "3"){
+            project_desc = "This is a description for project 3";
+        }
+
+        return(
+            <div className="container">
+                <h1>Projects Page, or go back to the <Link to="/">Home</Link> page.</h1>
+                <p>Here's the projects page.</p>
+
+                <br /><br />
+
+                <button
+                    style={this.state.project === "1" ? clicked : null}
+                    name="project"
+                    value={this.state.project === "1" ? "none" : "1"}
+                    onClick={this.handleChange}
+                >Project 1</button>
+                <button
+                    style={this.state.project === "2" ? clicked : null}
+                    name="project"
+                    value={this.state.project === "2" ? "none" : "2"}
+                    onClick={this.handleChange}
+                >Project 2</button>
+                <button
+                    style={this.state.project === "3" ? clicked : null}
+                    name="project"
+                    value={this.state.project === "3" ? "none" : "3"}
+                    onClick={this.handleChange}
+                >Project 3</button>
+
+                {this.state.project != "none" ?
+                <div><hr /><p>{project_desc}</p><hr /></div> :
+                <div></div>
+                }
+                <br /><br />
+                <p>And then some more things go here. :)</p>
+            </div>
+        )
+    }
 }
+
+export default ProjectsPage;
