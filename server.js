d56bbce0dec0870455b4fdbb5d5e94911e91296f
Duzz <rhubbard@student.unimelb.edu.au>
2020-10-13 10:49:09 +1100

    Cleaning repo

--- server.js
+++ server.js
@@ -26,6 +26,8 @@ const login = require('./routes/login');
 const name = require('./routes/name');
 
 // Middleware setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(methodOverride());
